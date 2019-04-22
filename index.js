891085041146d601ec41f3a1b959b9dd65869636
beschwa <naraiswar@gmail.com>
2019-04-22 14:21:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+// Code your solution in this fconst companyNameile!
+// let companyName = "Scuber"
+const companyName = "Scuber";
+
+let mostProfitableNeighborhood = "Chelsea";
+
+let companyCeo = "Susan Smith";
